ac65d652cdaf95a4d0b6db0515911eda1d96add1
Shawn Baugh II <shawnbaughcreative@gmail.com>
2017-04-24 19:55:04 -0500

    Changed settings.py file

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(va!q*%20qi&gk7_^wo34#a0js9srg1&@@7=a_jcd645b_1ask'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
